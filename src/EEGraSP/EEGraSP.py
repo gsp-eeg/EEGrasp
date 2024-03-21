a7cc7a8dfd5aab6dea5d9762c51b82f7b3ab8bfe
julioRodino <julio.rodino@ug.uchile.cl>
2024-03-21 13:14:44 +0000

    Rearanged structure for package building. Now EEGRaSP can be installed thorugh pip.

--- src/EEGraSP/EEGraSP.py
+++ src/EEGraSP/EEGraSP.py
@@ -20,12 +20,12 @@ class EEGraSP():
 
         for dim in range(pos.shape[1]):
             # Compute the component corresponding to each dimension. Add it to the array
-            W += np.square(pos[:,dim][:,None]-pos[:,dim][None,:])
+            W += np.power(pos[:,dim][:,None]-pos[:,dim][None,:],2)
         W = np.sqrt(W)
         
         return W
     
-    def compute_weights(self,method='Euclidean'):
+    def compute_distance(self,W=None,method='Euclidean'):
         """
         input: method for computing the distance.
         output: weight to be used for the graph computation
@@ -36,7 +36,8 @@ class EEGraSP():
         
         self.W = W
 
-    def compute_graph(self,W = None, method='NN',k=5,theta=.2,):
+
+    def compute_graph(self,W = None, method='NN',k=5,epsilon=0.1,theta=.2,):
         """"
         input: if W is passed, then the graph is computed. 
         Otherwise the graph will be computed with self.W
@@ -58,6 +59,7 @@ class EEGraSP():
             if method=='NN':
                 G = graphs.NNGraph(W,NNtype='knn',k=k)
             elif method=='Gaussian':
+                
                 G = graphs.Graph(np.exp(-W**2) / (2*theta**2))
 
             self.G = G
@@ -123,22 +125,31 @@ if __name__ == '__main__':
     left = epochs['left'].average()
     right = epochs['right'].average()
 
-    left.plot(gfp=True)
-    right.plot(gfp=True)
+    fig_left = left.plot(gfp=True)
+    fig_right = right.plot(gfp=True)
+    
+    fig_left.savefig('left_erp.png')
+    fig_right.savefig('rihgt_erp.png')
+
     plt.show()
 
     # %% Plot topology maps
+    
+    # Times to plot
+    times = np.arange(0.2,1.2,.2).round(1)
+    window = 0.1 # Window length for averaging
+    # Look vor vmin and vmax to share scale
+    all_data = np.hstack([left.get_data(),right.get_data()])
+    vmin = np.amin(all_data.reshape(-1)) * 1e6
+    vmax = np.amax(all_data.reshape(-1)) * 1e6
+    vlim = (vmin,vmax)
 
-    fig,axs = plt.subplots(2,6)
+    fig,axs = plt.subplots(2,len(times)+1,figsize=(8,4))
     fig.suptitle('Mental Imaginery',size=16)
     fontdic = {'size':12}
-    
-    times = np.array([.2,.3,.4,.5,.6])
-    window = 0.1
-    all_data = np.hstack([left.get_data(),right.get_data()])
-    vmin = np.amin(all_data.reshape(-1)) * 1e6 # minimum in time dim
-    vmax = np.amax(all_data.reshape(-1)) * 1e6# maximum in time dim
-    vlim = (vmin,vmax)
+
+
+    # Make plots and titles
     fig.text(.3,.87,'Left Hand',fontdic)
     fig = left.plot_topomap(times,average=window,axes=axs[0,:],
                             vlim=(vmin,vmax))
@@ -146,90 +157,113 @@ if __name__ == '__main__':
     fig = right.plot_topomap(times,average=window,axes=axs[1,:],
                              vlim=(vmin,vmax))
     
-    # Change subplot title
-    [ax.set_title(r'{} $\pm {}$ s'.format(t,window),size=9) for ax,t in zip(axs[:,:-1].flatten(),times.repeat(2))]
+    # Change subplots titles
+    for ax,t in zip(axs[:,:-1].flatten('F'),times.repeat(2)):
+        ax.set_title(r'{} $\pm {}$ s'.format(t,window),size=9)
     
-    plt.tight_layout()
-    plt.show()
+    fig.tight_layout()
+    fig.savefig('LR_topoplots.png')
+    fig.show()
 
     # %% Animate topomaps
         
     times = np.arange(0,1.1,0.01)
     
+    # Animation for left-hand imaginery
     fig,anim = left.animate_topomap(times=times,frame_rate=15,blit=False)
-    #anim.save('left.gif',fps=15)
     fig.suptitle('Left Motor Imaginery')
-    fig,anim = right.animate_topomap(times=times,frame_rate=15,blit=False)
-    #anim.save('right.gif', fps=15)
+    # Uncomment to save
+    #anim.save('left.gif',fps=15)
     
+    # Animation for right-hand imaginery
+    fig,anim = right.animate_topomap(times=times,frame_rate=15,blit=False)
     fig.suptitle('Right Motor Imaginery')
+    # Uncomment to save
+    #anim.save('right.gif', fps=15)
 
 
-# %% Initialize EEGraph class
+    # %% Initialize EEGraph class
     
     eegsp = EEGraSP(right,EEG_pos,ch_names)
-    eegsp.compute_weights() # Calculate distance between electrodes
+    eegsp.compute_distance() # Calculate distance between electrodes
+    W = eegsp.W.copy()
+
+    # Plot euclidean distance between electrodes
+    fig,ax = plt.subplots()
+    ax.set_title('Electrode Distance')
+    im = ax.imshow(W,cmap='Reds')
+    fig.colorbar(im,label='Euclidean Distance')
     
-    plt.imshow(eegsp.W,cmap='Reds')
-    plt.colorbar(label='Euclidean Distance')
-    plt.show()
+    # Uncomment to save
+    #fig.savefig('euc_dist.png')
+    fig.show()
 
-# %% Binarize W based on the histogram's probability mass
+    # %% Binarize W based on the histogram's probability mass
     
-    def init():
-        
-        return (dot,im)
-    
-    def update(frame):
-        
-        val = np.sort(vec_W)[frame]
-        p = prob[frame]
-
-        th_W = W > val # Keep distances lower than the threshold
-        np.fill_diagonal(th_W,0) # No self loops
-
-        dot.set_offsets([val,p])  
-        im.set_data(th_W) 
-
-        axs[1,1].clear()
-        G = graphs.Graph(th_W)
-        G.set_coordinates()
-        G.plot(ax=axs[1,1])
-        
-
-        return (dot,im)
-
-
     W = eegsp.W.copy()
     tril_idx = np.tril_indices(len(W),-1)
     vec_W = W[tril_idx] 
     count,bins = np.histogram(vec_W,bins=len(vec_W),density=False)
     
     prob = np.cumsum(count/len(vec_W))
-
+    th_W = W > 0
+    
     # Initiate figure
-    fig,axs = plt.subplots(2,2,figsize=(7,7))
+    fig,axs = plt.subplots(2,2,figsize=(10,9))
     axs[0,0].set_title('Cumulative Distribution Function')
     axs[0,0].set_xlabel('Euc. Distance')
     axs[0,0].set_ylabel('Probability')
 
-    lines, = axs[0,0].plot(np.sort(vec_W),prob)
-    dot = axs[0,0].scatter(np.sort(vec_W)[0],prob[0])
-    hist = axs[1,0].hist(vec_W,bins=len(vec_W),density=True)
-    th_W = W > 0
+    lines, = axs[0,0].plot(np.sort(vec_W),prob,color='black')
+    dot = axs[0,0].scatter(np.sort(vec_W)[0],prob[0],c='red')
+
+    hist = axs[1,0].hist(vec_W,bins=20,density=True,color='teal')
+    vline = axs[1,0].axvline(np.amin(vec_W),color='purple')
+    axs[1,0].set_title('Histogram')
+    axs[1,0].set_xlabel('Euc. Distance')
+    axs[1,0].set_ylabel('Probability Density')
+
     im = axs[0,1].imshow(th_W,cmap='gray')
-    fig.colorbar(im,ax=axs[0,1])
+    axs[0,1].set_xlabel('Electrode')
+    axs[0,1].set_ylabel('Electrode')
+    axs[0,1].set_title('Adjacency Matrix')
+    cbar = fig.colorbar(im,ax=axs[0,1],ticks = [0,1])
+    cbar.ax.set_yticklabels(['Unconnected','Connected'])
+
+    fig.tight_layout()
+
+    # Define function for animation
+    def update(frame):
+        
+        val = np.sort(vec_W)[frame]
+        p = prob[frame]
+
+        th_W = W <= val # Keep distances lower than the threshold
+        np.fill_diagonal(th_W,0) # No self loops
+
+        dot.set_offsets([val,p])  
+        im.set_data(th_W) 
+        vline.set_data([[val,val],[0,1]])
+
+        axs[1,1].clear()
+        G = graphs.Graph(th_W)
+        G.set_coordinates()
+        G.plot(ax=axs[1,1])
+
+        return (dot,im)
 
     anim = animation.FuncAnimation(fig,update,
-                                   frames=range(len(prob))[::20],
+                                   frames=np.arange(len(prob))[::8],
                                    interval=1,blit=False,
-                                   cache_frame_data=True)
+                                   cache_frame_data=False)
+    
+    # Uncomment to save animation 
+    #anim.save('G_thr.gif',fps=30)
 
-# %% Compute graph based on nearest neighbors based on euc. distance
+    
+    # %% Compute graph based on nearest neighbors based on euc. distance
     
     data = epochs['left'].get_data()
-
-    vk = np.arange(3,10) # Number of nearest neighbors to try
 
     nchannels = data.shape[1]
     nsamples = data.shape[2]
@@ -242,8 +276,25 @@ if __name__ == '__main__':
     mask[missing_idx] = False
     measures[:,~mask,:] = np.nan
     
+    # %% Graph based on gaussian kernel
+
+    epsilon = np.arange(0.04,0.2,0.01)
+    for e in epsilon:
+
+        G = graphs.NNGraph(EEG_pos,'radius',rescale=False,epsilon=e)
+        W = G.W.toarray()
+
+        fig,axs = plt.subplots(1,2,figsize=(7,4))
+        
+        im = axs[0].imshow(W,'gray',vmin=0,vmax=1)
+        fig.colorbar(im,cmap='jet')
+
+        G.set_coordinates()
+        G.plot(ax=axs[1])
+
     # %% Interpolate signal
 
+    vk = np.arange(3,10)
     # Allocate error matrix of len(vk) x epochs x timepoints
     error = np.zeros([len(vk),measures.shape[0]])
     recovery = np.zeros([len(vk),nepochs,nsamples])
@@ -257,7 +308,7 @@ if __name__ == '__main__':
             # Reconstruct every timepoint
             for iii,t in enumerate(epoch.T):
 
-                # Recover a signal                
+                # Recover a signal
                 recovery[i,ii,iii] = learning.regression_tikhonov(eegsp.G, t, mask, tau=0)[missing_idx]
             
             error[i,ii] = (np.linalg.norm(data[ii,missing_idx,:] - recovery[i,ii,:]))
