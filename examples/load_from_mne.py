6984d33360bc4387e8d98d18cdae64b9beff38c9
Julio Rodino <jrodino14@gmail.com>
2024-07-17 14:07:10 -0400

    create script to use as example to load data from mne with EEGRASP

---
+++ examples/load_from_mne.py
@@ -0,0 +1,7 @@
+"""Use mne epochs to import data and calculate the distance between electrodes."""
+
+# %% Load Packages
+import mne
+import numpy as np
+import matplotlib.pyplot as plt
+from eegrasp import EEGrasp
