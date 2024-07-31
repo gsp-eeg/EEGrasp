067a8a0e154c255dee77286c8d1a633212e4d3fe
Lucas Cortés <lucascortesg@gmail.com>
2024-07-31 17:19:42 -0400

    Moves large functions to their own modules

--- eegrasp/eegrasp.py
+++ eegrasp/eegrasp.py
@@ -14,7 +14,6 @@ import mne
 from .viz import plot_graph
 
 
-
 class EEGrasp():
     """Class containing functionality to analyze EEG signals.
 
@@ -101,40 +100,37 @@ class EEGrasp():
 
         return is_mne
 
-
     def euc_dist(self, pos):
         """ Calculates euclidean distance.
         %(eegrasp.utils.euc_dist)
         """
         from .utils import euc_dist
         return euc_dist(pos)
-    
 
     def compute_distance(self, coordinates=None, method='Euclidean', normalize=True):
         """ Computes distance.
         %(eegrasp.utils.compute_distance)
         """
         from .utils import compute_distance
-        self.distances = compute_distance(coordinates=coordinates, method=method, normalize=normalize, coord0=self.coordinates)
+        self.distances = compute_distance(
+            coordinates=coordinates, method=method, normalize=normalize, coord0=self.coordinates)
         return self.distances
-
 
     def gaussian_kernel(self, x, sigma=0.1):
         """ Calculates Gaussian Kernel.
         %(eegrasp.graph_creation.gaussian_kernel)
         """
-        from .graph_creation import gaussian_kernel
+        from .graph import gaussian_kernel
         return gaussian_kernel(x, sigma)
-        
 
     def compute_graph(self, W=None, epsilon=.5, sigma=.1, distances=None, graph=None, coordinates=None):
         """ Computes graph.
         %(eegrasp.graph_creation.compute_graph)
         """
-        from .graph_creation import compute_graph
-        self.graph, self.graph_weights = compute_graph(W=W, epsilon=epsilon, sigma=sigma, distances=distances, graph=graph, coordinates=coordinates, distances0=self.distances)
+        from .graph import compute_graph
+        self.graph, self.graph_weights = compute_graph(
+            W=W, epsilon=epsilon, sigma=sigma, distances=distances, graph=graph, coordinates=coordinates, distances0=self.distances)
         return self.graph
-        
 
     def interpolate_channel(self, missing_idx: int | list[int] | tuple[int], graph=None, data=None):
         """ Interpolates channel.
@@ -143,341 +139,34 @@ class EEGrasp():
         from .interpolate import interpolate_channel
         return interpolate_channel(missing_idx, graph=graph, data=data, graph0=self.graph, data0=self.data)
 
-
-    def _return_results(self, error, signal, vparameter, param_name):
-        """Function to wrap results into a dictionary.
-
-        Parameters
-        ----------
-        error : ndarray
-            Errors corresponding to each tried parameter.
-        vparameter : ndarray
-            Values of the parameter used in the fit function.
-        signal : ndarray
-            Reconstructed signal.
-
-        Notes
-        -----
-        In order to keep everyting under the same structure this function
-        should be used to return the results of any self.fit_* function.
-        """
-        best_idx = np.argmin(np.abs(error))
-        best_param = vparameter[best_idx]
-
-        results = {'error': error,
-                   'signal': signal,
-                   f'best_{param_name}': best_param,
-                   f'{param_name}': vparameter}
-
-        return results
-
-    def _vectorize_matrix(self, mat):
-        """
-        Vectorize a simetric matrix using the lower triangle.
-
-        Returns
-        -------
-        mat : ndarray.
-            lower triangle of mat
-        """
-        tril_indices = np.tril_indices(len(mat), -1)
-        vec = mat[tril_indices]
-
-        return vec
-
     def fit_epsilon(self, missing_idx: int | list[int] | tuple[int], data=None,
                     distances=None, sigma=0.1):
         """Find the best distance to use as threshold.
-
-        Parameters
-        ----------
-        missing_idx : int
-            Index of the missing channel. Not optional.
-        data : ndarray | None
-            2d array of channels by samples. If None, the function will use the
-            data computed in the instance of the class (`self.data`). Default
-            is `None`.
-        distances : ndarray | None.
-            Unthresholded distance matrix (2-dimensional array). It can be
-            passed to the instance of the class or as an argument of the
-            method. If None, the function will use the distance computed in the
-            instance of the class (`self.distances`). Default is `None`.
-        sigma : float
-            Parameter of the Gaussian Kernel transformation. Default is 0.1.
-
-        Returns
-        -------
-        results : dict
-            Dictionary containing the error, signal, best_epsilon and epsilon
-            values.
-
-        Notes
-        -----
-        It will itterate through all the unique values of the distance matrix.
-        data : 2-dimensional array. The first dim. is Channels
-        and second is time. It can be passed to the instance class or the method
+        %(eegrasp.graph.fit_epsilon)
         """
-        # Check if values are passed or use the instance's
-        if isinstance(distances, type(None)):
-            distances = self.distances.copy()
-        if isinstance(data, type(None)):
-            data = self.data.copy()
-
-        if isinstance(distances, type(None)) or isinstance(data, type(None)):
-            raise TypeError('Check data or W arguments.')
-
-        # Vectorize the distance matrix
-        dist_tril = self._vectorize_matrix(distances)
-
-        # Sort and extract unique values
-        vdistances = np.sort(np.unique(dist_tril))
-
-        # Create time array
-        time = np.arange(data.shape[1])
-
-        # Mask to ignore missing channel
-        ch_mask = np.ones(data.shape[0]).astype(bool)
-        ch_mask[missing_idx] = False
-
-        # Simulate eliminating the missing channel
-        signal = data.copy()
-        signal[missing_idx, :] = np.nan
-
-        # Allocate array to reconstruct the signal
-        all_reconstructed = np.zeros([len(vdistances), len(time)])
-
-        # Allocate Error array
-        error = np.zeros([len(vdistances)])
-
-        # Loop to look for the best parameter
-        for i, epsilon in enumerate(tqdm(vdistances)):
-
-            # Compute thresholded weight matrix
-            graph = self.compute_graph(distances, epsilon=epsilon, sigma=sigma)
-
-            # Interpolate signal, iterating over time
-            reconstructed = self.interpolate_channel(
-                missing_idx=missing_idx, graph=graph, data=signal)
-            all_reconstructed[i, :] = reconstructed[missing_idx, :]
-
-            # Calculate error
-            error[i] = np.linalg.norm(
-                data[missing_idx, :]-all_reconstructed[i, :])
-
-        # Eliminate invalid distances
-        valid_idx = ~np.isnan(error)
-        error = error[valid_idx]
-        vdistances = vdistances[valid_idx]
-        all_reconstructed = all_reconstructed[valid_idx, :]
-
-        # Find best reconstruction
-        best_idx = np.argmin(np.abs(error))
-        best_epsilon = vdistances[np.argmin(np.abs(error))]
-
-        # Save best result in the signal array
-        signal[missing_idx, :] = all_reconstructed[best_idx, :]
-
-        # Compute the graph with the best result
-        graph = self.compute_graph(distances, epsilon=best_epsilon,
-                                   sigma=sigma
-                                   )
-
-        results = self._return_results(error, signal, vdistances, 'epsilon')
-        return results
+        from .graph import fit_epsilon
+        fit_epsilon(missing_idx=missing_idx, data=data,
+                    distances=distances, sigma=sigma, data0=self.data, distances0=self.distances)
 
     def fit_sigma(self, missing_idx: int | list[int] | tuple[int], data=None,
                   distances=None, epsilon=0.5, min_sigma=0.1, max_sigma=1.,
                   step=0.1):
-        """Find the best parameter for the gaussian kernel.
-
-        Parameters
-        ----------
-        missing_idx : int | list | tuple
-            Index of the missing channel.
-        data : ndarray | None
-            2d array of channels by samples. If None, the function will use the
-            data computed in the instance of the class (`self.data`).
-        distances : ndarray | None
-            Distance matrix (2-dimensional array). It can be passed to the
-            instance of the class or as an argument of the method. If None, the
-            function will use the distance computed in the instance of the
-            class (`self.distances`).
-        epsilon : float
-            Maximum distance to threshold the array. Default is 0.5.
-        min_sigma : float
-            Minimum value for the sigma parameter. Default is 0.1.
-        max_sigma : float
-            Maximum value for the sigma parameter. Default is 1.
-        step : float
-            Step for the sigma parameter. Default is 0.1.
-
-        Notes
-        -----
-        Look for the best parameter of sigma for the gaussian kernel. This is
-        done by interpolating a channel and comparing the interpolated data to
-        the real data. After finding the parameter the graph is saved and
-        computed in the instance class. The distance threshold is maintained.
-
+        """ Find the best parameter for the gaussian kernel.
+        %(eegrasp.graph.fit_sigma)
         """
-
-        # Check if values are passed or use the class instance's
-        if isinstance(distances, type(None)):
-            distances = self.distances.copy()
-        if isinstance(data, type(None)):
-            data = self.data.copy()
-
-        if isinstance(distances, type(None)) or isinstance(data, type(None)):
-            raise TypeError('Check data or W arguments.')
-
-        # Create array of parameter values
-        vsigma = np.arange(min_sigma, max_sigma, step=step)
-
-        # Create time array
-        time = np.arange(data.shape[1])
-
-        # Mask to ignore missing channel
-        ch_mask = np.ones(data.shape[0]).astype(bool)
-        ch_mask[missing_idx] = False
-
-        # Simulate eliminating the missing channel
-        signal = data.copy()
-        signal[missing_idx, :] = np.nan
-
-        # Allocate array to reconstruct the signal
-        all_reconstructed = np.zeros([len(vsigma), len(time)])
-
-        # Allocate Error array
-        error = np.zeros([len(vsigma)])
-
-        # Loop to look for the best parameter
-        for i, sigma in enumerate(tqdm(vsigma)):
-
-            # Compute thresholded weight matrix
-            graph = self.compute_graph(epsilon=epsilon, sigma=sigma)
-
-            # Interpolate signal, iterating over time
-            reconstructed = self.interpolate_channel(
-                missing_idx=missing_idx, graph=graph, data=signal)
-            all_reconstructed[i, :] = reconstructed[missing_idx, :]
-
-            # Calculate error
-            error[i] = np.linalg.norm(
-                data[missing_idx, :]-all_reconstructed[i, :])
-
-        # Eliminate invalid trials
-        valid_idx = ~np.isnan(error)
-        error = error[valid_idx]
-        vsigma = vsigma[valid_idx]
-        all_reconstructed = all_reconstructed[valid_idx, :]
-
-        # Find best reconstruction
-        best_idx = np.argmin(np.abs(error))
-        best_sigma = vsigma[np.argmin(np.abs(error))]
-
-        # Save best result in the signal array
-        signal[missing_idx, :] = all_reconstructed[best_idx, :]
-
-        # Compute the graph with the best result
-        graph = self.compute_graph(distances, epsilon=epsilon,
-                                   sigma=best_sigma
-                                   )
-
-        self.graph = graph
-
-        results = self._return_results(error, signal, vsigma, 'sigma')
-
-        return results
+        from .graph import fit_sigma
+        return fit_sigma(missing_idx=missing_idx, data=data,
+                         distances=distances, epsilon=epsilon, min_sigma=min_sigma, max_sigma=max_sigma,
+                         step=step, data0=self.data, distances0=self.distances)
 
     def learn_graph(self, Z=None, a=0.1, b=0.1,
                     gamma=0.04, maxiter=1000, w_max=np.inf,
                     mode='Average'):
-        """Learn the graph based on smooth signals.
-
-        Parameters
-        ----------
-        Z : ndarray
-            Distance between the nodes. If not passed, the function will try to
-            compute the euclidean distance using `self.data`. If `self.data` is
-            a 2d array it will compute the euclidean distance between the
-            channels. If the data is a 3d array it will compute a Z matrix per
-            trial, assuming the first dimension in data is
-            trials/epochs. Depending on the mode parameter, the function will
-            average distance matrizes and learn the graph on the average
-            distance or return a collection of adjacency matrices. Default is
-            None.
-        a : float
-            Parameter for the graph learning algorithm, this controls the
-            weights of the learned graph. Bigger a -> bigger weights in
-            W. Default is 0.1.
-        b : float
-            Parameter for the graph learning algorithm, this controls the
-            density of the learned graph. Bigger b -> more dense W. Default is
-            0.1.
-        mode : string
-            Options are: 'Average', 'Trials'. If 'average', the function
-            returns a single W and Z.  If 'Trials' the function returns a
-            generator list of Ws and Zs. Default is 'Average'.
-
-        Returns
-        -------
-        W : ndarray
-            Weighted adjacency matrix or matrices depending on mode parameter
-            used. If run in 'Trials' mode then Z is a 3d array where the first
-            dim corresponds to trials.
-        Z : ndarray.
-            Used distance matrix or matrices depending on mode parameter
-            used. If run in 'Trials' mode then Z is a 3d array where the first
-            dim corresponds to trials.
+        """ Learns graph using PyGSP2.
+        %(eegrasp.graph.learn_graph)
         """
-
-        # If no distance matrix is given compute based on
-        # data's euclidean distance
-        if Z is None:
-            data = self.data.copy()
-
-        # Check if data contains trials
-        if data.ndim == 3:
-
-            Zs = np.zeros((data.shape[0], data.shape[1], data.shape[1]))
-
-            # Check if we want to return average or trials
-            if mode == 'Trials':
-
-                Ws = np.zeros(
-                    (data.shape[0], data.shape[1], data.shape[1]))
-                for i, d in enumerate(tqdm(data)):
-                    # Compute euclidean distance
-                    Z = self.euc_dist(d)
-
-                    W = graph_learning.graph_log_degree(
-                        Z, a, b, gamma=gamma, w_max=w_max, maxiter=maxiter)
-                    W[W < 1e-5] = 0
-
-                    Ws[i, :, :] = W.copy()
-                    Zs[i, :, :] = Z.copy()
-
-                return Ws, Zs
-
-            elif mode == 'Average':
-
-                for i, d in enumerate(tqdm(data)):
-                    # Compute euclidean distance
-                    Zs[i, :, :] = self.euc_dist(d)
-
-                Z = np.mean(Zs, axis=0)
-                W = graph_learning.graph_log_degree(
-                    Z, a, b, gamma=gamma, w_max=w_max, maxiter=maxiter)
-                W[W < 1e-5] = 0
-
-                return W, Z
-        else:
-            Z = self.euc_dist(data)
-
-            W = graph_learning.graph_log_degree(
-                Z, a, b, gamma=gamma, w_max=w_max, maxiter=maxiter)
-            W[W < 1e-5] = 0
-
-            return W, Z
+        from .graph import learn_graph
+        return learn_graph(Z=Z, a=a, b=b, gamma=gamma, maxiter=maxiter, w_max=w_max, mode=mode, data0=self.data)
 
     def plot(self, graph=None, signal=None, coordinates=None, labels=None, montage=None,
              colorbar=True, axis=None, clabel='Edge Weights', kind='topoplot', show_names=True, **kwargs):
